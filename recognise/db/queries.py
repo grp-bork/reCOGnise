bd16c3f7727889c5cbc2ce9156897e402cddc8e8
Christian Schudoma <cschu1981@gmail.com>
2023-03-07 15:13:12 +0100

    fix: use gzip open instead of open

--- recognise/db/queries.py
+++ recognise/db/queries.py
@@ -12,7 +12,7 @@ def get_sequences_from_cluster(mongo_db_str, cluster_id, seqfile):
 	with gzip.open(seqfile, "wt") as genes_out:
 		for record in fr_db.samples.find({'fr13_cluster': cluster_id}):
 			genes_file = f"{record['analysis_path']}/ref_genome_called_genes/{record['sample_id']}.genes.fa.gz"
-			with open(genes_file, "rt") as genes_in:
+			with gzip.open(genes_file, "rt") as genes_in:
 				genes_raw = genes_in.read()
 				n_genes += genes_raw.count(">")
 				print(genes_raw, file=genes_out, end="" if genes_raw[-1] == "\n" else "\n")
