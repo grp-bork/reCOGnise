b9dd8dbd033ceb62c3b11c7a2765ce2d28ff5674
Christian Schudoma <cschu1981@gmail.com>
2023-03-13 09:45:53 +0100

    fix: obtain paths from database before copying files

--- recognise/db/queries.py
+++ recognise/db/queries.py
@@ -9,9 +9,12 @@ def get_sequences_from_cluster(mongo_db_str, cluster_id, seqfile):
 	fr_db = client["progenomes"]
 
 	n_genes = 0
+	files = 
 	with gzip.open(seqfile, "wt") as genes_out:
 		for record in fr_db.samples.find({'fr13_cluster': cluster_id}):
 			genes_file = f"{record['analysis_path']}/ref_genome_called_genes/{record['sample_id']}.genes.fa.gz"
+			files.append(genes_file)
+		for genes_file in files:
 			with gzip.open(genes_file, "rt") as genes_in:
 				genes_raw = genes_in.read()
 				n_genes += genes_raw.count(">")
