a78b9486244d1b28b9918c1af31b037be9b83c50
Christian Schudoma <christian.schudoma@embl.de>
2026-04-27 19:13:21 +0200

    add gzip support

--- recognise/utils.py
+++ recognise/utils.py
@@ -1,11 +1,24 @@
+import gzip
 import itertools as it
 
 
+GZIP_MAGIC = b"\x1f\x8b\x08"
+
 def read_fasta(f):
+
     with open(f) as _in:
+        is_gzipped = _in.read(3) == GZIP_MAGIC
+
+    open_f = gzip.open if is_gzipped else open
+
+    with open_f(f, "rt", encoding="utf-8",) as _in:
         gen = it.groupby(_in, key=lambda x:x[0]==">",)
         for is_header, item in gen:
             if is_header:
                 seqid = next(item).rstrip()[1:]
                 _, seqs = next(gen)
                 yield seqid, "".join(s.rstrip() for s in seqs)
+
+
+
+    
